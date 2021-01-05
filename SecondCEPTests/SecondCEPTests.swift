ada17ecd5d63a45ba91f237ea50172b27be78d6c
RJ <r.j.chomik@gmail.com>
2021-01-05 16:38:42 +0000

    init commit

---
+++ SecondCEPTests/SecondCEPTests.swift
@@ -0,0 +1,34 @@
+//
+//  SecondCEPTests.swift
+//  SecondCEPTests
+//
+//  Created by The App Experts on 05/01/2021.
+//  Copyright © 2021 The App Experts. All rights reserved.
+//
+
+import XCTest
+@testable import SecondCEP
+
+class SecondCEPTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
